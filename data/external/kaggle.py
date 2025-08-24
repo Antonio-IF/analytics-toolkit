ed2a7c8945aaacdd08ce573992655424fb5f0509
Juan Antonio Mugica <138845558+Juan-An-ML@users.noreply.github.com>
2025-08-24 17:58:00 -0600

    "Function to download datasets from Kaggle! =) "

--- data/external/kaggle.py
+++ data/external/kaggle.py
@@ -2,16 +2,12 @@ import kagglehub
 import shutil
 import os
 
-# Descargar dataset (se guarda en un directorio temporal)
-path = kagglehub.dataset_download("adilshamim8/predict-calorie-expenditure")
+def kaggle_dataset(path_name,dest_path): 
+    path = kagglehub.dataset_download(path_name)
+    for file_name in os.listdir(path):
+        full_file_name = os.path.join(path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, dest_path)
+    return print("Files copy complete on:", dest_path)
 
-# Definir carpeta destino
-dest_path = "data/raw/calories-expenditure"
-
-# Copiar todos los archivos descargados a "data/raw/predict-calorie-expenditure"
-for file_name in os.listdir(path):
-    full_file_name = os.path.join(path, file_name)
-    if os.path.isfile(full_file_name):
-        shutil.copy(full_file_name, dest_path)
-
-print("Archivos copiados a:", dest_path)
+kaggle_dataset("adilshamim8/predict-calorie-expenditure", "data/raw/calories-expenditure")
