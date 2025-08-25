fb8db885f1a5dadcf4f24a7254be1d77b89145ed
Juan Antonio Mugica <138845558+Juan-An-ML@users.noreply.github.com>
2025-08-24 18:10:25 -0600

    ""

--- data/external/kaggle.py
+++ data/external/kaggle.py
@@ -2,15 +2,31 @@ import kagglehub
 import shutil
 import os
 
-def kaggle_dataset(path_name,dest_path): 
+def kaggle_dataset(path_name,dest_path, create_dir=False): 
+    """
+    Args:
+        path_name (str): The name of the dataset on Kaggle to be downloaded, in the format 'username/dataset_name'.
+        dest_path (str): The destination path where the downloaded files will be copied.
+        create_dir (bool): A flag that determines whether to create the destination directory if it does not exist. 
+                           Defaults to False.
+    """
+
+    if create_dir:
+        os.makedirs(dest_path, exist_ok=True)
+    else: 
+        if not os.path.exists(dest_path):
+            raise FileNotFoundError(f"The dest path in: {dest_path} doesn't exist & create_dir=False")     
+
     path = kagglehub.dataset_download(path_name)
+
     for file_name in os.listdir(path):
         full_file_name = os.path.join(path, file_name)
         if os.path.isfile(full_file_name):
             shutil.copy(full_file_name, dest_path)
     return print("Files copy complete on:", dest_path)
 
-# 1st 
-# Dataset
+# Using function......
+
+# 1st Dataset;
 # kaggle_dataset("adilshamim8/predict-calorie-expenditure", "data/raw/calories-expenditure")
 
